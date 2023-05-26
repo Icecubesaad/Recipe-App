ae5dfa65743cd7ccb9a45d4612358242983e631e
icecube_saad <102737032+Icecubesaad@users.noreply.github.com>
2023-05-27 03:04:14 +0500

    Update database.js

--- server/database.js
+++ server/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const url = 'mongodb://127.0.0.1:27017/Recipes';
+const url = 'YOUR MONGODB STRING';
 const mongo_connect = ()=>{
     mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
