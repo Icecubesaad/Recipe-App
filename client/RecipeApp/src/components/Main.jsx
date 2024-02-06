f1eca872e6c277385631b2f8ec4f8972cb86857f
Icecubesaad <saad.qad418@gmail.com>
2024-02-06 14:06:05 +0500

    commit

--- client/RecipeApp/src/components/Main.jsx
+++ client/RecipeApp/src/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import image2 from "../static/image-06.png";
+import image2 from "../assets/static/image-06.png";
 import { Link } from "react-router-dom";
 import animate from "animate.css"
 const Main = () => {
@@ -8,7 +8,7 @@ const Main = () => {
       <div className="content">
         <div className="text">
           <div className="Heading-text">Your own chef</div>
-          <div className="Heading-para" className="animate__animated animate__bounce animate__faster" >
+          <div className="Heading-para  animate__animated animate__bounce animate__faster"  >
             Get in touch with the most unique and interesting reciepies from
             people all around the world
           </div>
